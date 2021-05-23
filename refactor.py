2d75906e07f4cb11fc7376a1cd7851dfad6d7c16
maverick <creativ999@yandex.ru>
2021-05-23 21:04:15 +0300

    refactor

--- refactor.py
+++ refactor.py
@@ -23,35 +23,32 @@ def query_paginate(link: str) -> list:
     return result
 
 
-def query_page(link: str, category: int):
+def query_page_author(link: str):
     """Данные с одной страницы"""
     query = requests.get(link)
     soup = BeautifulSoup(query.text, 'html.parser')
-    result = soup.find_all('div', {'class': 'center_live'})
-    result_author = soup.select("div.center_live span")
-    count = len(result_author)
+    result = soup.select(f'div.center_live span')
+    response = [item.get_text().strip() for item in result]
+    return response
+
+
+def query_page_dict(link: str, category: int):
+    """Данные с одной страницы"""
+    query = requests.get(link)
+    soup = BeautifulSoup(query.text, 'html.parser')
+    result = soup.select(f'div.center_live')
     response = []
-    counter = 0
     for item in range(len(result)):
-        new_str = str(result[item].get_text()).strip()
-        try:
-            new_author = str(result_author[counter].get_text()).strip()
-            text = new_str[:-len(new_author)]
-        except IndexError as error:
-            print(f'{error} - нету автора! - {text}')
         response.append({
             'model': config.MODEL,
             'fields': {
-                'content': text,
-                'author': new_author,
+                'content': result[item].get_text().strip(),
+                'author': '',
                 'category': category,
                 'time_create': DATE_NOW,
                 'time_update': DATE_NOW
             }
         })
-        counter += 1
-        if counter > count:
-            break
     return response
 
 
@@ -61,10 +58,20 @@ while True:
     file_name = str(input('Введите название файла '))
     parse_off = int(input('Продолжать работу? '))
     page = query_paginate(url)
-    all_data = []
+    list_content = []
+    list_author = []
     for link_query in page:
-        q = query_page(link_query, cat_id)
-        all_data.extend(q)
-    json_writen(all_data, file_name)
+        content_list = query_page_dict(link_query, cat_id)
+        author_list = query_page_author(link_query)
+        author_list = list(set(author_list))
+        list_content.extend(content_list)
+        list_author.extend(author_list)
+    for index in range(len(list_content)):
+        for author in list_author:
+            if author in list_content[index]['fields']['content']:
+                list_content[index]['fields']['content'] = list_content[index]['fields']['content'][:-len(author)]
+                list_content[index]['fields']['author'] = author
+
+    json_writen(list_content, file_name)
     if parse_off == 0:
         break
---
+++ test.py
@@ -0,0 +1,11 @@
+result_author = [1, 2, 3, 4, 5, 6, 7]
+
+conuter = 0
+
+cont = 9
+
+for item in result_author:
+    print(result_author[conuter])
+    conuter += 1
+    if conuter > cont:
+        break
